73a3d0c975972dadfda75eb16a4f04ee38be8aa8
Sergey_Sukhanov <Sergey.bv26@gmail.com>
2021-11-01 16:55:20 +0300

    Выполнено практическое задание к 6 уроку

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,15 +1,29 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.template.loader import render_to_string
 from mainapp.models import Product
 from basketapp.models import Basket
 
 
+@login_required
 def basket(request):
-    context = {}
+    title = 'корзина'
+    basket_items = Basket.objects.filter(user=request.user)
+
+    context = {
+        'title': title,
+        'basket_items': basket_items
+    }
+
     return render(request, 'basketapp/basket.html', context)
 
 
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
 
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -23,7 +37,32 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
-    context = {}
+    basket_item = get_object_or_404(Basket, pk=pk)
+    basket_item.delete()
 
-    return render(request, 'basketapp/basket.html', context)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=int(pk))
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+
+        basket_items = Basket.objects.filter(user=request.user)
+
+        context = {
+            'basket_items': basket_items,
+        }
+
+        result = render_to_string('basketapp/includes/inc_basket_list.html', context)
+
+        return JsonResponse({'result': result})
