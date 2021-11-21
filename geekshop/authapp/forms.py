bff2c0edb8c79d14cbeb3e5356fcf41e34cc2917
Sergey_Sukhanov <Sergey.bv26@gmail.com>
2021-11-21 09:51:57 +0300

    Выполнено практическое задание 2 урока 2 курса

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, User
 from django import forms
 from django.utils.timezone import now
 
-from authapp.models import ShopUser
+from authapp.models import ShopUser, ShopUserProfile
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -68,3 +68,15 @@ class ShopUserEditForm(UserChangeForm):
         if data < 18:
             raise forms.ValidationError('Вы слишком молоды')
         return data
+
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ('tagline', 'about_me', 'gender')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
