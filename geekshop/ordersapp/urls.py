a0a4dea7525dab71f156e4603bed1fb7b18a0cc2
Sergey_Sukhanov <Sergey.bv26@gmail.com>
2021-11-28 10:39:23 +0300

    Выполнено 4задание 2 курса

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('update/<int:pk>/', ordersapp.OrderUpdateView.as_view(), name='update'),
     path('create/', ordersapp.OrderCreateView.as_view(), name='create'),
     path('delete/<int:pk>/', ordersapp.OrderDeleteView.as_view(), name='delete'),
-    path('forming/complete/<int:pk>/', ordersapp.order_forming_complete, name='order_forming_complete')
+    path('forming/complete/<int:pk>/', ordersapp.order_forming_complete, name='order_forming_complete'),
+    path('update/<int:pk_ord>/product/added/<int:pk>/', ordersapp.order_added_product, name='')
 ]
