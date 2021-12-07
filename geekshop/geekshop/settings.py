370c09c03a09eea5eebc42480bfb1c35a8faed28
ssukhanov <sergey.bv26@gmail.com>
2021-12-07 20:44:49 +0300

    Added django_extensions

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'template_profiler_panel',
+    'django_extensions',
     'social_django',
     'basketapp',
     'authapp',
